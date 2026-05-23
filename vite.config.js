473f8ccc90e0196afcd8814069d4b3f043ba6a5f
kevdeveloperfront <kevdeveloperfront@gmail.com>
2026-05-23 16:29:39 -0400

    feat: migración a pnpm y despliegue automático con github actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/portafolio/', // Crucial para que los estilos y JS carguen en GitHub Pages
   plugins: [
     tailwindcss(),
   ],
